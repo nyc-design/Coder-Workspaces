82fbc233c484522d2fe80cc812c604390cb51114
nyc-design <neil@tapiavala.com>
2025-12-09 08:22:13 +0000

    updated coder versions for coder tasks

--- workspace-modules/codex/main.tf
+++ workspace-modules/codex/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = ">= 2.7"
+      version = ">= 2.13.0"
     }
   }
 }
--- workspace-templates/repo-envbuilder-agent/main.tf
+++ workspace-templates/repo-envbuilder-agent/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = ">=2.4.0"
+      version = ">= 2.13.0"
     }
     docker = {
       source = "kreuzwerker/docker"
